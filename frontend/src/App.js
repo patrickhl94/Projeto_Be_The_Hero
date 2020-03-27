af1bad790f588dc44c654bfb4d095151d0c72b71
Patrick Lima <patrickhl94@hotmail.com>
2020-03-26 23:39:16 -0300

    Insert routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import './global.css'
-import Logon from './pages/Logon/index';
+import Routes from './routes';
 
 function App() {
   return (
-    <Logon />
+    <Routes />
   );
 }
 
