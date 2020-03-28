f50b6307710bf1a3010598781d2b5af2f5a59476
Patrick Lima <patrickhl94@hotmail.com>
2020-03-28 18:02:37 -0300

    Updated App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Olá Mundo!</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
