955756d931972334f44b672e0ea2dac7d1fcfd2f
Patrick Lima <patrickhl94@hotmail.com>
2020-03-28 18:01:59 -0300

    Created routes.js

---
+++ mobile/src/routes.js
@@ -0,0 +1,23 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react';
+
+const AppStack = createStackNavigator();
+
+import Incidents from './pages/Incidents';
+import Detail from './pages/Detail';
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+
+      <AppStack.Navigator screenOptions={{headerShown: false}} >
+
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Detail" component={Detail} />
+
+      </AppStack.Navigator>
+
+    </NavigationContainer>
+  );
+}
