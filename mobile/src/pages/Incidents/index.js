e68eb0e4b2256ac144ae8bb7dd80560e754fd5de
Patrick Lima <patrickhl94@hotmail.com>
2020-03-28 18:03:08 -0300

    Created index.js

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons'
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+import styles from './styles';
+
+export default function Incidents() {
+  return (
+    <View style={styles.container} >
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText} >
+          Total de <Text style={styles.headerTextBold}> 0 casos</Text>.
+      </Text>
+      </View>
+
+      <Text style={styles.title}>Bem vindo!</Text>
+      <Text style={styles.description}>
+        Escolha um dos casos abaixo e salve o dia.
+      </Text>
+
+      <FlatList
+        style={styles.incidentsList}
+        data={[1, 2, 3]}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+
+            <Text style={styles.incidentProperty}>Caso:</Text>
+            <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+            <Text style={styles.incidentProperty}>Valor</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={() => { }} >
+              <Text style={styles.detailsButtonText} > Ver mais detalhes </Text>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+
+    </View>
+  );
+};
